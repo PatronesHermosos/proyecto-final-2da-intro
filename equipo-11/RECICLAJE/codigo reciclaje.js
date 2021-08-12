0ac9e3203d765183913896975db8c8d30b848932
bridgetaa1 <85418931+bridgetaa1@users.noreply.github.com>
2021-08-12 18:09:50 -0500

    Add files via upload
    Con los cambios del día de hoy en la asesoría, aún no corre :(

--- equipo-11/RECICLAJE/codigo reciclaje.js
+++ equipo-11/RECICLAJE/codigo reciclaje.js
@@ -1,8 +1,7 @@
-'<'script>
+<script>
 var logo = document.getElementById('reciclaje')
 deg = 1;
-setInterval (function()   {logo.style.transformOrigin = "10, 10";
-logo.style.transform = "rotateX (" + deg + "deg)";
+setInterval (function() {logo.style.transformOrigin = "10, 10";logo.style.transform = "rotateX (" + deg + "deg)";
 logo.style.transform = "rotateY (" + deg + "deg)";
-deg = (deg + 1) % 360}, 10;
+deg = (deg + 1) % 360,10;}
 </script>
